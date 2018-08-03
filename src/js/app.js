3f0abdb65bd065870b421dd2ca8ec8f28f046f6f
idready <you_sombo@hotmail.fr>
2018-08-03 21:17:24 +0200

    Finished Ui

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,12 @@ import { store } from './store/store';
 import { addIngredient } from './actions/ingredient';
 import { addRecipe } from './actions/recipe'
 
-// {
-//     recipe: 'Omelette',
-//     name: 'Egg',
-//     quantity: 2
-// }
+// UI
+import { loadUi } from './ui';
 
 // Tests
 store.dispatch(addRecipe('Ravioli'));
 store.dispatch(addIngredient('Ravioli', 'Mayonnaise', '50g'));
 store.dispatch(addIngredient('Ravioli', 'Sel', '10g'));
+
+loadUi();
